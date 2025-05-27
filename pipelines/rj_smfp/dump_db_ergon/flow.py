754a5f6addcf88c457728e616472087ebdaccf6a
d116626 <d116626@gmail.com>
2025-05-27 07:17:29 -0300

    chore: inject bd credential

--- pipelines/rj_smfp/dump_db_ergon/flow.py
+++ pipelines/rj_smfp/dump_db_ergon/flow.py
@@ -1,5 +1,8 @@
 from prefect import flow, task
-from pipelines.utils.infisical import get_database_username_and_password_from_secret
+from pipelines.utils.infisical import (
+    get_database_username_and_password_from_secret,
+    inject_bd_credentials,
+)
 from pipelines.utils.utils import log
 
 
@@ -9,8 +12,14 @@ def get_database_username_and_password_from_secret_tastk(secret_path: str):
     return secrets
 
 
+@task
+def inject_bd_credentials_task(enviroment: str = "prod"):
+    inject_bd_credentials(enviroment=enviroment)
+
+
 @flow(log_prints=True)
 def ergon(secret_path: str):
+    inject_bd_credentials_task(enviroment="prod")
 
     secrets = get_database_username_and_password_from_secret_tastk(
         secret_path=secret_path
