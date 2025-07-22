43ff64e31d615a7f0bbcbca331a5151fa4d629a6
d116626 <d116626@gmail.com>
2025-07-22 15:59:55 -0300

    chore: inject bd creds

--- pipelines/rj_iplanrio__equipamentos_arcgis/flow.py
+++ pipelines/rj_iplanrio__equipamentos_arcgis/flow.py
@@ -20,7 +20,7 @@ def rj_iplanrio__equipamentos_arcgis(
     dataset_id: str = "brutos_equipamentos",
     table_id: str = "unidades_saude_poligonos_datario",
 ):
-    rename_flow_run = rename_current_flow_run_task(new_name=f"{dataset_id}.{table_id}")
+    rename_flow_run = rename_current_flow_run_task(new_name=table_id)
     crd = inject_bd_credentials_task(environment="prod")  # noqa
     path = download_equipamentos_from_datario(url=url, crs=crs)
     create_table_and_upload_to_gcs_task(
