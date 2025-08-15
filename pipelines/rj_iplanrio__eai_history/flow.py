4ad1d2f042cf3fc108174ea4ed6a82f898d95090
d116626 <d116626@gmail.com>
2025-08-15 17:11:08 -0300

    fix: add dump mode

--- pipelines/rj_iplanrio__eai_history/flow.py
+++ pipelines/rj_iplanrio__eai_history/flow.py
@@ -15,17 +15,22 @@ from pipelines.rj_iplanrio__eai_history.tasks import fetch_history_data
 
 @flow(log_prints=True)
 def rj_iplanrio__eai_history(
-    last_update: str = "2025-08-29",
+    last_update: str = "2025-07-29",
     session_timeout_seconds: Optional[int] = 3600,
     use_whatsapp_format: bool = False,
     dataset_id: str = "brutos_eai_logs",
     table_id: str = "history",
 ):
-    name = rename_current_flow_run_task(new_name=last_update)
-    cred = inject_bd_credentials_task()
+    rename_current_flow_run_task(new_name=last_update)
+    inject_bd_credentials_task()
     data_path = fetch_history_data(
         last_update=last_update,
         session_timeout_seconds=session_timeout_seconds,
         use_whatsapp_format=use_whatsapp_format,
     )
-    create_table_and_upload_to_gcs_task(data_path=data_path, dataset_id=dataset_id, table_id=table_id)
+    create_table_and_upload_to_gcs_task(
+        data_path=data_path,
+        dataset_id=dataset_id,
+        table_id=table_id,
+        dump_mode="append",
+    )
