2defd15b13d8c91907b5f59f655250bd76b14c7e
Patricia Bongiovanni Catandi <62657143+patriciacatandi@users.noreply.github.com>
2025-09-22 18:36:12 +0000

    testing stop flow when there is no new data

--- pipelines/rj_crm__api_wetalkie/tasks.py
+++ pipelines/rj_crm__api_wetalkie/tasks.py
@@ -15,7 +15,8 @@ import pandas as pd
 import requests
 from iplanrio.pipelines_utils.logging import log
 from prefect import task
-from prefect.states import SKIP
+from prefect.exceptions import TerminationSignal
+from prefect.states import Completed
 from pytz import timezone
 
 from pipelines.rj_crm__api_wetalkie.utils.tasks import (
@@ -57,7 +58,14 @@ def get_attendances(api: object) -> pd.DataFrame:
     all_attendances = response_data["data"]["items"]
     if not all_attendances:
         log("No attendances found in the Wetalkie API", level="warning")
-        raise SKIP("No attendances found in the Wetalkie API")
+        new_state = Completed(
+            message="Flow new state"        )
+        
+        # Raise a TerminationSignal with the new state
+        raise TerminationSignal(
+            message="Flow terminated",
+            state=new_state
+        )
 
     for item in all_attendances:
         data.append(
