6cf4067b5379514a679844a1525c9dce25f93f21
hellcassius <caiorogerio.santos@gmail.com>
2025-10-06 12:15:08 -0300

    remove iplan content, add smtr configs to deploy

--- pipelines/rj_smas__cadunico/env.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import os
-
-# if file .env exists, load it
-if os.path.exists("pipelines/rj_smas__cadunico/.env"):  # noqa
-    import dotenv
-
-    dotenv.load_dotenv(dotenv_path="pipelines/rj_smas__cadunico/.env")
