64b593b4bb8ebefb369c4553728c41c49f267434
Joao Pedro Oliveira <joaopsantos@pm.me>
2025-10-08 12:30:26 -0300

    fix dep

--- pipelines/rj_iplanrio__sisbicho_images/utils/tasks.py
+++ pipelines/rj_iplanrio__sisbicho_images/utils/tasks.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
-import base64
-
 from iplanrio.pipelines_utils.logging import log
+from datetime import datetime
+from typing import Literal
+from prefect import task
+import pandas as pd
+import base64
+import uuid
+import os
+
 
 # Mapeamento de magic numbers
 MAGIC_NUMBERS = {
@@ -36,4 +42,57 @@ def detect_and_decode(data_b64: str) -> bytes:
         log("Decodificação dupla necessária.")
         return step2
 
-    raise ValueError("Não foi possível identificar o tipo de arquivo após 1 ou 2 decodificações.")
+    raise ValueError(
+        "Não foi possível identificar o tipo de arquivo após 1 ou 2 decodificações."
+    )
+
+
+@task
+def create_date_partitions(
+    dataframe,
+    partition_column: str = None,
+    file_format: Literal["csv", "parquet"] = "csv",
+    root_folder="./data/",
+):
+    """
+    Create date partitions for a DataFrame and save them to disk.
+    """
+
+    if partition_column is None:
+        partition_column = "data_particao"
+        dataframe[partition_column] = datetime.now().strftime("%Y-%m-%d")
+    else:
+        dataframe[partition_column] = pd.to_datetime(
+            dataframe[partition_column], errors="coerce"
+        )
+        dataframe["data_particao"] = dataframe[partition_column].dt.strftime("%Y-%m-%d")
+        if dataframe["data_particao"].isnull().any():
+            raise ValueError("Some dates in the partition column could not be parsed.")
+
+    dates = dataframe["data_particao"].unique()
+    dataframes = [
+        (
+            date,
+            dataframe[dataframe["data_particao"] == date].drop(
+                columns=["data_particao"]
+            ),
+        )
+        for date in dates
+    ]
+
+    for _date, _dataframe in dataframes:
+        partition_folder = os.path.join(
+            root_folder,
+            f"ano_particao={_date[:4]}/mes_particao={_date[5:7]}/data_particao={_date}",
+        )
+        os.makedirs(partition_folder, exist_ok=True)
+
+        file_folder = os.path.join(partition_folder, f"{uuid.uuid4()}.{file_format}")
+
+        if file_format == "csv":
+            _dataframe.to_csv(file_folder, index=False)
+        elif file_format == "parquet":
+            _dataframe.to_parquet(file_folder, index=False)
+
+    log(f"Files saved on {root_folder}")
+    return root_folder
