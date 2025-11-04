1508f62b480d447365a636ecd80d313a8bd5cf25
Joao Pedro Oliveira <joaopsantos@pm.me>
2025-11-04 11:57:16 -0300

    import log

--- pipelines/rj_iplanrio__alertario_previsao_24h/flow.py
+++ pipelines/rj_iplanrio__alertario_previsao_24h/flow.py
@@ -5,6 +5,7 @@ Flow para extrair previsões meteorológicas do AlertaRio e carregar no BigQuery
 
 from iplanrio.pipelines_utils.bd import create_table_and_upload_to_gcs_task
 from iplanrio.pipelines_utils.env import inject_bd_credentials_task
+from iplanrio.pipelines_utils.logging import log
 from iplanrio.pipelines_utils.prefect import rename_current_flow_run_task
 from prefect import flow
 
