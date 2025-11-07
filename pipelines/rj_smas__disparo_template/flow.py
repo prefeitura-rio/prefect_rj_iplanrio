a173d60f4ebc5ee7f935abce2fb36a5cc5cbd5cc
patriciacatandi <patriciabcatandi@gmail.com>
2025-11-07 17:13:28 -0300

    remove parameter

--- pipelines/rj_smas__disparo_template/flow.py
+++ pipelines/rj_smas__disparo_template/flow.py
@@ -93,7 +93,6 @@ def rj_smas__disparo_template(
         destinations=destinations,
         query=query,
         billing_project_id=billing_project_id,
-        query_processor_name=query_processor_name,
     )
 
     validated_destinations = skip_flow_if_empty(
