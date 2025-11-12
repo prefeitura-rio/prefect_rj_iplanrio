22f6728ade79f287a3a06a90ead9120a59fe67d1
patriciacatandi <patriciabcatandi@gmail.com>
2025-11-12 10:25:22 -0300

    adding flow

--- pipelines/rj_crm_registry__disparo_template/flow.py
+++ pipelines/rj_crm_registry__disparo_template/flow.py
@@ -28,6 +28,7 @@ from pipelines.rj_crm_registry__disparo_template.utils.dispatch import (
     create_dispatch_dfr,
     create_dispatch_payload,
     dispatch,
+    format_query,
     get_destinations,
     remove_duplicate_phones,
 )
@@ -53,7 +54,8 @@ def rj_crm_registry__disparo_template(
     dump_mode: str | None = None,
     test_mode: bool | None = True,
     query: str | None = None,
-    query_processor_name: str | None = "template",
+    query_processor_name: str | None = None,
+    query_replacements: dict | None = None,
     sleep_minutes: int | None = 5,
     infisical_secret_path: str = "/wetalkie",
     whitelist_percentage: int = 30,
@@ -90,9 +92,19 @@ def rj_crm_registry__disparo_template(
 
     api_status = check_api_status(api)
 
+    if query_replacements:
+        query_complete = format_query(
+            raw_query=query,
+            replacements=query_replacements,
+            query_processor_name=query_processor_name,
+        )
+    else:
+        query_complete = query
+    print(f"\n⚠️  Query dispatch:\n{query_complete}")
+
     destinations_result = get_destinations(
         destinations=destinations,
-        query=query,
+        query=query_complete,
         billing_project_id=billing_project_id,
     )
 
