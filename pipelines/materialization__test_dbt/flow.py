90668cc13ac96efa7bda83f6f66c0cc7d8caed57
hellcassius <caiorogerio.santos@gmail.com>
2025-12-18 16:27:51 -0300

    add debug

--- pipelines/materialization__test_dbt/flow.py
+++ pipelines/materialization__test_dbt/flow.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from prefect_dbt import PrefectDbtRunner
 
 from prefect import flow, task
@@ -11,9 +12,12 @@ def materialization__test_dbt():
     Flow to execute dbt materialization tests.
     """
     inject_bd_credentials_task()
+    profiles_dir = '/opt/prefect/pipelines_v3/queries'
+    project_dir = '/opt/prefect/pipelines_v3/queries'
+    files = [f for f in Path(profiles_dir).glob('**/*.sql')]
+    print(f"Files in profiles_dir ({profiles_dir}): {[str(f) for f in files]}")
 
     runner = PrefectDbtRunner()
-    profiles_dir = 'opt/prefect/pipelines_v3/queries'
-    project_dir = 'opt/prefect/pipelines_v3/queries'
+    
     run_result = runner.invoke(['compile', '--select', 'models/br_rj_riodejaneiro_brt_gps','--project-dir', project_dir, '--profiles-dir', profiles_dir])
     print(run_result)  # You can log or process the result as needed
