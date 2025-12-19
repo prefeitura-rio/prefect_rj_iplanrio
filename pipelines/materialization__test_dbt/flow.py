5368f95c5077cb2740df2a3be61ca736e34b2a1f
hellcassius <caiorogerio.santos@gmail.com>
2025-12-19 15:43:31 -0300

    add set envs to dbt test flow

--- pipelines/materialization__test_dbt/flow.py
+++ pipelines/materialization__test_dbt/flow.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from prefect_dbt import PrefectDbtRunner, PrefectDbtSettings
 
 from prefect import flow, task
+from os import environ
 
 from iplanrio.pipelines_utils.env import inject_bd_credentials_task
 
@@ -14,6 +15,11 @@ def materialization__test_dbt():
     inject_bd_credentials_task()
     profiles_dir = '/opt/prefect/pipelines_v3/queries'
     project_dir = '/opt/prefect/pipelines_v3/queries'
+
+    #set envs
+    environ['DBT_PROFILES_DIR'] = profiles_dir
+    environ['DBT_PROJECT_DIR'] = project_dir
+
     files = [f for f in Path(profiles_dir).glob('**/*.yml')]
     print(f"Files in profiles_dir ({profiles_dir}): {[str(f) for f in files]}")
 
