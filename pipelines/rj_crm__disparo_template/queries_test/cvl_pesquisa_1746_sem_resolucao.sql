ceada714872873a81c3285beee6a0476605dc36e
patriciacatandi <patriciabcatandi@gmail.com>
2026-07-08 16:27:57 -0300

    fix: changing sf scheduler

---
+++ pipelines/rj_crm__disparo_template/queries_test/cvl_pesquisa_1746_sem_resolucao.sql
@@ -0,0 +1,14 @@
+-- Query de teste para CVL Pesquisa 1746 Sem Resolução
+SELECT 
+    "5511984677798" AS telefone,
+    "12345678901" AS SubscriberKey,
+    "Usuario Teste Sem Resolucao" AS nome_sobrenome,
+    "Varrição de logradouro" AS solicitacao,
+    "Central 1746" AS canal
+UNION ALL
+SELECT 
+    "5521989190512" AS telefone,
+    "98765432100" AS SubscriberKey,
+    "Outro Teste Sem Resolucao" AS nome_sobrenome,
+    "Reparo de buraco na pista" AS solicitacao,
+    "Aplicativo 1746" AS canal
