726104f5c3bd7d8c486f4ca517a204a518b6c701
tricktx <patrick.tx@hotmail.com.br>
2026-09-01 16:18:13 -0300

    fix: duplicidade docstring

--- pipelines/rj_cor__precipitacao_alertario_sftp/flow.py
+++ pipelines/rj_cor__precipitacao_alertario_sftp/flow.py
@@ -1,4 +1,4 @@
-"""Flow para coleta de precipitação do AlertaRio via SFTP em landing zone GCS.."""
+"""Flow para coleta de precipitação do AlertaRio via SFTP em landing zone GCS."""
 
 from prefect import flow
 from iplanrio.pipelines_utils.env import inject_bd_credentials_task
