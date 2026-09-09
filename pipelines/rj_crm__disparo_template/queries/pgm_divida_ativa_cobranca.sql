aa11c9dbf52b07c4ebd4514b55d195fc62c112aa
patriciacatandi <patriciabcatandi@gmail.com>
2026-09-09 12:06:38 -0300

    altera dentro da query para receber o valor das duas hsm

--- pipelines/rj_crm__disparo_template/queries/pgm_divida_ativa_cobranca.sql
+++ pipelines/rj_crm__disparo_template/queries/pgm_divida_ativa_cobranca.sql
@@ -39,7 +39,7 @@ SELECT celulares_validos.*
 FROM celulares_validos
 LEFT JOIN `rj-crm-registry.brutos_salesforce.status_disparo` sd
     ON sd.cpf = celulares_validos.cpf
-    AND sd.nome_hsm = '{nome_hsm_cobranca_placeholder}'
+    AND sd.nome_hsm IN ({nome_hsm_cobranca_placeholder})
     AND sd.processado_datahora >= DATETIME_SUB(CURRENT_DATETIME("America/Sao_Paulo"), INTERVAL 150 DAY)
     AND sd.data_particao >= DATE_SUB(CURRENT_DATE("America/Sao_Paulo"), INTERVAL 151 DAY)
 LEFT JOIN `rj-crm-registry.brutos_wetalkie_staging.fluxo_atendimento_*` fl
