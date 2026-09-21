e984ca7fb4550b6c11bf49d419795b59f3170441
d116626 <d116626@gmail.com>
2026-09-21 15:22:08 -0300

    fix: Correct migration comment for DBT Transform Flow from Prefect 1.4 to 3.0

--- pipelines/rj_ssm__run_dbt/flow.py
+++ pipelines/rj_ssm__run_dbt/flow.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # ruff: disable=C0301,PTH118,PTH109,PTH110,PTH103,B904,B904,PTH118,PTH112
 """
-Migrated DBT Transform Flow from Prefect 1.4 to 3.0..
+Migrated DBT Transform Flow from Prefect 1.4 to 3.0...
 """
 
 import os
